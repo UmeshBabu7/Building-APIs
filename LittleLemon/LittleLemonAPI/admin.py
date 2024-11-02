6b1935f23634de1d1949fe8411b089af33aec663
Umesh <umeshtamang129@gmail.com>
2024-11-02 12:15:07 +0545

    creating a models and admin interface

--- LittleLemon/LittleLemonAPI/admin.py
+++ LittleLemon/LittleLemonAPI/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category,MenuItem
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'inventory', 'category')
+    
