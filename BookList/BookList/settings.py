683861616ebea6e254c5866b7a2e71bb2763296c
Umesh <umeshtamang129@gmail.com>
2024-11-02 11:54:13 +0545

    using debug toolbar

--- BookList/BookList/settings.py
+++ BookList/BookList/settings.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
 
     # Third Party App
     'rest_framework',
+
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -42,6 +44,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'BookList.urls'
@@ -138,3 +141,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
